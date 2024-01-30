b02f9f850e7650c03a49ec155bf83e18c26ae448
andrecampll <andrevictor50@gmail.com>
2024-01-29 21:02:57 -0300

    feat: adds pagination button components 🚀

--- src/app/(dashboard)/orders/page.tsx
+++ src/app/(dashboard)/orders/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from 'next'
 
+import { Pagination } from '@/components/pagination'
 import {
   Table,
   TableBody,
@@ -20,29 +21,31 @@ export default function Orders() {
     <>
       <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-bold tracking-tighter">Orders</h1>
-      </div>
-      <div className="space-y-2.5">
-        <OrderTableFilters />
+        <div className="space-y-2.5">
+          <OrderTableFilters />
 
-        <div className="rounded-md border">
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead className="w-[64px]"></TableHead>
-                <TableHead className="w-[140px]">Identifier</TableHead>
-                <TableHead className="w-[180px]">Ordered at</TableHead>
-                <TableHead className="w-[140px]">Status</TableHead>
-                <TableHead>Customer</TableHead>
-                <TableHead className="w-[140px]">Total</TableHead>
-                <TableHead className="w-[164px]"></TableHead>
-                <TableHead className="w-[132px]"></TableHead>
-              </TableRow>
-            </TableHeader>
+          <div className="rounded-md border">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-[64px]"></TableHead>
+                  <TableHead className="w-[140px]">Identifier</TableHead>
+                  <TableHead className="w-[180px]">Ordered at</TableHead>
+                  <TableHead className="w-[140px]">Status</TableHead>
+                  <TableHead>Customer</TableHead>
+                  <TableHead className="w-[140px]">Total</TableHead>
+                  <TableHead className="w-[164px]"></TableHead>
+                  <TableHead className="w-[132px]"></TableHead>
+                </TableRow>
+              </TableHeader>
 
-            <TableBody>
-              <OrderTableRow />
-            </TableBody>
-          </Table>
+              <TableBody>
+                <OrderTableRow />
+              </TableBody>
+            </Table>
+          </div>
+
+          <Pagination pageIndex={0} totalCount={105} perPage={10} />
         </div>
       </div>
     </>
